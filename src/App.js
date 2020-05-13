8970e400c0a6f895b5bf9bf58f2ec334abd9220f
nbrooks89 <nbrooks2389@gmail.com>
2020-05-13 13:53:15 -0700

    new new

--- src/App.js
+++ src/App.js
@@ -1,20 +1,71 @@
 import React, { Component } from 'react'
+import Square from './components/Square'
 import './App.css'
 
 class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
+      squares: ["?", "?", "?", "?", "?", "?", "?", "?", "?"],
+      bomb: "bomb",
+      treausure: "treasure"
     }
   }
 
+
+
+handleChange = (event) => {
+  // a method that will take the value from an input and save it in the state key "name"
+  this.setState({ squares: event.target.value })
+}
+
+ // findStuff = () => {
+ //   if (this.state.squares === this.state.bomb){
+ //     alert (" BOMB")
+ //   }else if( this.state.squares === this.state.treasure ){
+ //    alert ("winnnah")
+ //   }
+ //  }
+
+
+//   randomTreasure = () => {
+//   let setTreasure = Math.floor(Math.random()* this.state.squares.length )
+//     this.setState({treasure:setTreasure})
+//
+//
+//     }
+//   randomBomb = () => {
+//   let setBomb = Math.floor(Math.random()* this.state.squares.length )
+//     this.setState({bomb:setBomb})
+// }
+
+
+
+  //if 1-7 = tree
+  // if else
+  // if else 8 = bomb
+  // if else 9 = Treasure
+
+
   render(){
+    let square = this.state.squares.map((value, index)=> {
+      return(
+        <Square
+          value={ value }
+          index={ index }
+        />
+      )
+    })
     return(
       <React.Fragment>
         <h1>Treasure Hunt App</h1>
+        <div>
+          { square }
+        </div>
+
       </React.Fragment>
     )
   }
 }
+
 export default App
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react'
-
+mport React, { Component } from 'react'
+import './Square.css'
 class Square extends Component{
+
+  // findStuff = () => {
+  //   if (this.state.squares === this.state.bomb){
+  //     alert (" BOMB")
+  //   }else if( this.state.squares === this.state.treasure ){
+  //    alert ("winnnah")
+  //   }
+  //  }
   render(){
     return(
       <React.Fragment>
+        <div id="square" >{ this.props.value }</div>
       </React.Fragment>
     )
   }
